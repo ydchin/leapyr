7348c6b07b65dcdbd067d712e8d122e0d71ff564
Yoon-orn Chin <dylanchin123@gmail.com>
2021-03-04 04:24:15 -0800

    Implemented checking if divisible by 400 or not

--- leapyr.py
+++ leapyr.py
@@ -3,5 +3,7 @@ def leapYear(year):
         if year % 100 == 0:
             if year % 400 == 0:
                 return str(year) + " is a leap year"
+            else:
+                return str(year) + " is not a leap year"
         else:
             return str(year) + " is a leap year"
