164265e179f32b5b87eee47170b43bffdf13f402
Yoon-orn Chin <dylanchin123@gmail.com>
2021-03-04 04:26:03 -0800

    Wrote third test, check for input 2003 .
    check if divisible by 4

--- test_leapyr.py
+++ test_leapyr.py
@@ -4,9 +4,12 @@ import leapyr
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(leapyr.leapYear(2000), "2000 is a leap year")
-        
+
     def test2(self):
         self.assertEqual(leapyr.leapYear(2100), "2100 is not a leap year")
+    
+    def test3(self):
+        self.assertEqual(leapyr.leapYear(2003), "2003 is not a leap year")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
