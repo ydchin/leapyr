5868ffb7d4230c021f9c62eb9c0c367b7f804c0f
Yoon-orn Chin <dylanchin123@gmail.com>
2021-03-04 04:14:05 -0800

    Initial commit

---
+++ test_leapyr.py
@@ -0,0 +1,9 @@
+import unittest
+import leapyr
+
+class TestCase(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(leapyr, "2000 is not a leap year")
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
