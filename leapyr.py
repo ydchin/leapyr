6bc63d0af7492a3b2e6dfafbef4a8e27ddca3439
Yoon-orn Chin <dylanchin123@gmail.com>
2021-03-04 04:20:57 -0800

    Implemented to check years divisible by 4, 100, and 400

--- leapyr.py
+++ leapyr.py
@@ -1,1 +1,5 @@
-
+def leapYear(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return str(year) + " is a leap year"
--- test_leapyr.py
+++ test_leapyr.py
@@ -3,8 +3,7 @@ import leapyr
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        self.assertEqual(leapyr, "2000 is a leap year")
-
+        self.assertEqual(leapyr.leapYear(2000), "2000 is a leap year")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
